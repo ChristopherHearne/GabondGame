85bad2f1ff51e0aa8ceaabd47b521ba2b63a9c82
christopherhearne <christofer.hearne@squeed.com>
2024-09-11 08:59:28 +0200

    changed title and name of application to correct game name

--- src/StartPage.tsx
+++ src/StartPage.tsx
@@ -6,7 +6,7 @@ interface StartPageProps {
 const StartPage: React.FC<StartPageProps> = ({ onStart }) => {
   return (
     <div className="start-page">
-      <h1>Welcome to Gabong</h1>
+      <h1>Welcome to Gabond</h1>
       <button onClick={onStart}>Start Game</button>
     </div>
   );
